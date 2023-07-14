1b62355ac38d4a9f78109f2b3ba7a20947c3ea6f
JudgeLevia <ponycandy666@126.com>
2023-07-14 15:32:41 +0800

    初始提交

---
+++ autograd/variable.h
@@ -0,0 +1,97 @@
+#if !defined(__TENSOR_H__)
+#define __TENSOR_H__
+
+//#include <boost/log/trivial.hpp>
+#include <fmt/format.h>
+#include <memory>
+
+namespace autograd {
+
+class Node;
+
+class Edge {
+  std::shared_ptr<Node> grad_fn_;
+  int input_nr_ = 0;
+
+public:
+  Edge() = default;
+  Edge(std::shared_ptr<Node> grad_fn, int input_nr)
+      : grad_fn_(grad_fn), input_nr_(input_nr) {}
+
+  std::shared_ptr<Node> grad_fn() { return grad_fn_; }
+
+  int input_nr() { return input_nr_; }
+
+  void set_grad_fn(std::shared_ptr<Node> grad_fn) { grad_fn_ = grad_fn; }
+};
+
+class Variable : public std::enable_shared_from_this<Variable> {
+  using T = float;
+
+  // Autograd Metadata
+  bool requires_grad_ = true;
+
+public:
+  T value_ = T();
+  T grad_ = T();
+  Edge gradient_edge_;
+
+  void set_gradient_edge(Edge &&gradient_edge);
+
+  Edge gradient_edge();
+
+  void add_grad(T grad_value) { grad_ += grad_value; }
+
+  std::shared_ptr<Variable> shared_ptr() { return shared_from_this(); }
+
+  Variable() = default;
+
+  Variable(T value) : value_(value) {}
+
+  Variable operator=(T value) { value_ = value; }
+
+  T grad() { return grad_; }
+  
+  T value() { return value_; }
+
+  void zero_grad() { grad_ = 0.0f; }
+
+  bool requires_grad() { return requires_grad_; }
+
+  void set_requires_grad(bool requires_grad) { requires_grad_ = requires_grad; }
+
+  std::string to_string() const {
+    return fmt::format("Variable @ {} value = {} grad = {}", fmt::ptr(this),
+                       value_, grad_);
+  }
+
+  std::shared_ptr<Variable> detach();
+  std::shared_ptr<Variable> log();
+  std::shared_ptr<Variable> relu();
+  std::shared_ptr<Variable> sigmoid();
+};
+
+std::shared_ptr<Variable> variable(float v);
+
+std::shared_ptr<Variable> operator+(std::shared_ptr<Variable> lhs,
+                                    std::shared_ptr<Variable> rhs);
+std::shared_ptr<Variable> operator-(std::shared_ptr<Variable> var);
+std::shared_ptr<Variable> operator-(std::shared_ptr<Variable> lhs,
+                                    std::shared_ptr<Variable> rhs);
+std::shared_ptr<Variable> operator*(std::shared_ptr<Variable> lhs,
+                                    std::shared_ptr<Variable> rhs);
+std::shared_ptr<Variable> operator/(std::shared_ptr<Variable> lhs,
+                                    std::shared_ptr<Variable> rhs);
+std::shared_ptr<Variable> operator^(std::shared_ptr<Variable> lhs,
+                                    std::shared_ptr<Variable> rhs);
+//以下是我的定义
+std::shared_ptr<Variable> operator+(std::shared_ptr<Variable> lhs, double rhs);
+std::shared_ptr<Variable> operator+(double lhs,std::shared_ptr<Variable> rhs);
+std::shared_ptr<Variable> operator-(std::shared_ptr<Variable> lhs, double rhs);
+std::shared_ptr<Variable> operator-(double lhs, std::shared_ptr<Variable> rhs);
+//下面为数乘
+std::shared_ptr<Variable> operator*(double lhs,std::shared_ptr<Variable> rhs);
+std::shared_ptr<Variable> operator*(std::shared_ptr<Variable> lhs, double rhs);
+} // namespace autograd
+
+#endif // __TENSOR_H__
---
+++ operators.cpp
@@ -0,0 +1,87 @@
+#include "autograd/operators.h"
+#include <cmath>
+
+namespace autograd {
+
+variable_list AccumulateGrad::apply(variable_list &&grads) {
+  auto grad = grads[0].value_;
+  if (auto ptr = variable_.lock()) {
+    ptr->grad_ += grad;
+  }
+  return variable_list();
+}
+
+variable_list AddBackward::apply(variable_list &&grads) {
+  auto grad = grads[0].value_;
+  variable_list grads_input{grad, grad};
+  return grads_input;
+}
+
+variable_list MulBackward::apply(variable_list &&grads) {
+  auto grad = grads[0].value_;
+  variable_list grads_input{other_->value_ * grad, self_->value_ * grad};
+  return grads_input;
+}
+
+variable_list DivBackward::apply(variable_list &&grads) {
+  auto grad = grads[0].value_;
+  variable_list grads_input{1.0f / other_->value_ * grad,
+                            -self_->value_ / (other_->value_ * other_->value_) *
+                                grad};
+  return grads_input;
+}
+
+variable_list SubBackward::apply(variable_list &&grads) {
+  auto grad = grads[0].value_;
+  variable_list grads_input{grad, -grad};
+  return grads_input;
+}
+
+variable_list PowBackward::apply(variable_list &&grads) {
+  auto grad = grads[0].value_;
+  auto xvalue = self_->value_;
+  auto yvalue = other_->value_;
+  variable_list grads_input{grad * yvalue * std::pow(xvalue, yvalue - 1),
+                            grad * std::pow(xvalue, yvalue) * std::log(xvalue)};
+  return grads_input;
+}
+
+variable_list LogBackward::apply(variable_list &&grads) {
+  auto grad = grads[0].value_;
+  auto value = self_->value_;
+  variable_list grads_input{grad / value};
+  return grads_input;
+}
+
+variable_list ReLUBackward::apply(variable_list &&grads) {
+  auto grad = grads[0].value_;
+  auto value = self_->value_;
+  float grad_value = value >= 0 ? grad : 0.0f;
+  variable_list grads_input{grad_value};
+  return grads_input;
+}
+
+variable_list NegBackward::apply(variable_list &&grads) {
+  auto grad = grads[0].value_;
+  variable_list grads_input{-grad};
+  return grads_input;
+}
+//以下为我的添加：
+variable_list AddBackwardConstant::apply(variable_list&& grads) {
+    auto grad = grads[0].value_;  
+    //这一项是指求导图到目前为止所积累的导数值，所以输出应该是本节点导数乘以这个值
+    //上面的NegBackward就是一个例子
+    variable_list grads_input{ grad };
+    //有几项variable变量参与这个运算,grads_input就有几项，对应顺序没有关系，系统会遍历每个variable的梯度
+    // 左操作数和右操作数哪个在前无所谓
+    //定义对自己的导数：按照本符号定义的输入顺序，处在第一个位置上的变量就是自己
+    return grads_input;
+    //这个应该可以通用给左+和右+常数
+}
+variable_list MulBackwardConstant::apply(variable_list&& grads) {
+    auto grad = grads[0].value_;
+    variable_list grads_input{ other_* grad};
+    return grads_input;
+}
+
+} // namespace autograd
